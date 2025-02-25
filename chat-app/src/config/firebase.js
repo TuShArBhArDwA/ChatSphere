dc4da6469a2681511fcad7ae54c9d9a07dbc83f6
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-26 03:07:03 +0530

    Create firebase.js

---
+++ chat-app/src/config/firebase.js
@@ -0,0 +1,14 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBJUk8qHtA_1JqKxFabqgRh8eZg-6cDUZk",
+  authDomain: "chat-sphere-gs.firebaseapp.com",
+  projectId: "chat-sphere-gs",
+  storageBucket: "chat-sphere-gs.firebasestorage.app",
+  messagingSenderId: "564622939570",
+  appId: "1:564622939570:web:2d1034f56b8590a08d80ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
