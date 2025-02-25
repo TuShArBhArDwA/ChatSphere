ee0406955128d67086324c85d47bee1d5c833661
TuShArBhArDwA <tusharbhardwaj2617@gmail.com>
2025-02-25 23:03:27 +0530

    sign up/login component

--- chat-app/src/pages/Chat/Chat.jsx
+++ chat-app/src/pages/Chat/Chat.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 import './Chat.css'
 const Chat = () => {
   return (
-    <div>
-      We are on the chat page
+    <div className='login'>
+      <img src="" alt="" className='logo'/>
     </div>
   )
 }
